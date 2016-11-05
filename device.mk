734fa3d20b1661f921116475e74469a0da770265
bshiznit <rvkasper@gmail.com>
2016-11-05 13:25:46 -0600

    Bump to v0.70 (supersu is back! sysinit is back

--- device.mk
+++ device.mk
@@ -322,7 +322,7 @@ PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/sdhci-tegra.3/by-name/VNR
 # overwrite product specific build properties
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
-    BUILD_DISPLAY_ID="Noougies-$(BUILD_ID)-v0.55"
+    BUILD_DISPLAY_ID="Noougies-$(BUILD_ID)-v0.70"
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
 $(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
